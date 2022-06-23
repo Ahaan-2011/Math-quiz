7d660bda067390afe69f2a69459007c2e642836f
Ahaan-2011 <96826290+Ahaan-2011@users.noreply.github.com>
2022-06-23 19:48:52 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -1,4 +1,8 @@
+player1_score= 0;
+player2_score= 0;
+
 function names () {
+    
 
 
 player1_name=localStorage.getItem("player1_name");
@@ -11,6 +15,8 @@ document.getElementById("player2_name").innerHTML=player2_name + " : ";
 document.getElementById("player_question").innerHTML="Question turn -" + player1_name;
 document.getElementById("player_answer").innerHTML="Answer turn -" + player2_name;
 }
+
+
 
 function send () {
 
@@ -28,4 +34,61 @@ document.getElementById("output").innerHTML=row;
 
 document.getElementById("number1").value="";
 document.getElementById("number2").value="";
+}
+
+
+question_turn="player1";
+answer_turn="player2";
+
+function check ()
+{
+
+get_answer= document.getElementById("input_check_box").value;
+
+if (get_answer==actual_answer) 
+{
+
+if (answer_turn=="player1") 
+{
+
+update_player1_score= player1_score + 1;
+document.getElementById("player1_score").innerHTML = update_player1_score;
+
+}
+
+else 
+{
+
+update_player2_score= player2_score + 1;
+document.getElementById("player2_score").innerHTML = update_player2_score;
+
+}
+}
+
+if (question_turn=="player1")
+{
+    question_turn="player2";
+    document.getElementById("player_question").innerHTML="Question Turn - " + player2_name;
+}
+
+else 
+{
+    question_turn="player1";
+    document.getElementById("player_question").innerHTML="Question Turn - " + player1_name;
+}
+
+
+if (answer_turn=="player1")
+{
+    answer_turn="player2";
+    document.getElementById("player_answer").innerHTML="answer Turn - " + player2_name;
+}
+
+else 
+{
+    answer_turn="player1";
+    document.getElementById("player_answer").innerHTML="answer Turn - " + player1_name;
+}
+
+
 }
